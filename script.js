251832af8454f7264ae54cb0b78ab5675fe14a6b
Efraim Rocha <95988659+Efrals@users.noreply.github.com>
2025-04-18 21:04:00 -0300

    Finalização do backend

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ class Calculator {
     this.clear();
   }
 
+  // Faz os cálculos das operações
   calculate() {
     let result;
 
@@ -41,11 +42,47 @@ class Calculator {
     this.previousOperand = "";
   }
 
+  //Deleta o último número
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  // Formata com vírgula e separa os números
+  formataDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+
+    //Verifica se o número é NaN e se for, retorna vazio
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("pt-BR", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    //Verifica se o número tem alguma parte decimal
+    if (decimalDigits != null) {
+      return `${integerDisplay},${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   //Guarda as operações
   choseOperation(operation) {
-    //Verifica se o valor atual está vazio
+    // Troca a operação se não há número atual e já existe uma operação
+    if (this.currentOperand === "" && this.previousOperand !== "") {
+      this.operation = operation;
+      return;
+    }
+
+    // Se não há número atual, não faz nada
     if (this.currentOperand === "") return;
-    //Verifica se o valor atual é vazio e se o valor anterior está vazio
+
+    // Verifica se há número anterior, antes de executar a operação anterior
     if (this.previousOperand !== "") {
       this.calculate();
     }
@@ -63,22 +100,28 @@ class Calculator {
     this.currentOperand = `${this.currentOperand}${number}`;
   }
 
+  //Limpa os valores
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
     this.operation = undefined;
   }
 
+  // Atualiza o display da calculadora
   updateDisplay() {
     this.previousOperandText.innerText = `${this.previousOperand} ${
       this.operation || ""
     }`;
-    this.currentOperandText.innerText = this.currentOperand;
+    this.currentOperandText.innerText = this.formataDisplayNumber(
+      this.currentOperand
+    );
   }
 }
 
+// Cria uma nova instância da calculadora
 const calculator = new Calculator(previousOperandText, currentOperandText);
 
+// Botões de números
 for (const numberButton of numberButtons) {
   numberButton.addEventListener("click", () => {
     calculator.appendNumber(numberButton.innerText);
@@ -86,7 +129,7 @@ for (const numberButton of numberButtons) {
   });
 }
 
-// Adiciona o evento de clique para os botões de operação
+// Botões de operação
 for (const operationButton of operationButtons) {
   operationButton.addEventListener("click", () => {
     calculator.choseOperation(operationButton.innerText);
@@ -94,8 +137,20 @@ for (const operationButton of operationButtons) {
   });
 }
 
-/// Adiciona o evento de clique para os botões de operação
+// Botão AC
 allClearButton.addEventListener("click", () => {
   calculator.clear();
+  calculator.updateDisplay();
+});
+
+// Botão de igual
+equalsButton.addEventListener("click", () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+});
+
+// Botão de delete
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
   calculator.updateDisplay();
 });
